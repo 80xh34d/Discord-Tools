8d542d13d1ce969def8022a8856c575a0bee9031
Boxhead <107728918+80xh34d@users.noreply.github.com>
2022-06-21 00:04:30 -0600

    Update discordutils.py
    added discord snowflake conversion function

--- scripts/discordutils.py
+++ scripts/discordutils.py
@@ -16,7 +16,12 @@ class DiscordUtils:
             }[user_os]
         )
 
-    def kill_discord(self, client: str) -> None:
+    def snowflake_to_date(snowflake: int) -> str:
+        timestamp = ((snowflake >> 22) + 1420070400000) / 1e3
+        print(timestamp)
+        return timestamp
+
+    def kill_discord(client: str) -> None:
         for proc in psutil.process_iter():
             if client.lower() in proc.name().lower():
                 try:
@@ -24,8 +29,10 @@ class DiscordUtils:
                 except (psutil.NoSuchProcess, psutil.AccessDenied):
                     pass
 
-    def run_discord(self, client: str) -> None:
+    def run_discord(client: str) -> None:
         executable = os.path.join(os.getenv("localappdata"), client, "Update.exe")
         args = f"{executable} --processStart {client}.exe"
         FNULL = open(os.devnull, "w")
         subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
+
+DiscordUtils.snowflake_to_date(988546298540085298)
